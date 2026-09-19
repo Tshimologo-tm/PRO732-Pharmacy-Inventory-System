a4b4399dfa3fba057844fac768e716ddcfa960f0
Tshimologo <Tshimologo@TshimologoMolotsi>
2026-09-20 01:38:33 +0200

    Rename Java package for HealthFirst Pharmacy

--- HealthFirstPIMS/src/main/java/za/co/healthfirstpims/MolotsiMedPIMS.java
+++ HealthFirstPIMS/src/main/java/za/co/healthfirstpims/MolotsiMedPIMS.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package za.co.molotsimedpims;
+package za.co.healthfirstpims;
 
 /**
  *
