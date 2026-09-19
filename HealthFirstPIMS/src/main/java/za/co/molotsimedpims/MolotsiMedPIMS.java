c305efa2c8b1c7f482d84cc85ee3d21a18f1023f
Tshimologo <Tshimologo@TshimologoMolotsi>
2026-09-19 23:15:14 +0200

    Set up HealthFirst Pharmacy project structure

---
+++ HealthFirstPIMS/src/main/java/za/co/molotsimedpims/MolotsiMedPIMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package za.co.molotsimedpims;
+
+/**
+ *
+ * @author Tshimologo
+ */
+public class MolotsiMedPIMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
