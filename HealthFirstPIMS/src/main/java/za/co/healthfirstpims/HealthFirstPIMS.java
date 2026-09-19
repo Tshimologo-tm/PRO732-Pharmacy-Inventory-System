389d7cc9e5dedbdc0c0335c1955e123e90cb6980
Tshimologo <Tshimologo@TshimologoMolotsi>
2026-09-20 01:56:58 +0200

    Rename main application class to HealthFirstPIMS

--- HealthFirstPIMS/src/main/java/za/co/healthfirstpims/HealthFirstPIMS.java
+++ HealthFirstPIMS/src/main/java/za/co/healthfirstpims/HealthFirstPIMS.java
@@ -8,7 +8,7 @@ package za.co.healthfirstpims;
  *
  * @author Tshimologo
  */
-public class MolotsiMedPIMS {
+public class HealthFirstPIMS {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
